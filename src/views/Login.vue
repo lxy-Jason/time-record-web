9855fac2dc24504ca9ad5649aff538e589fe17c3
Debbl <me@aiwan.run>
2022-10-04 15:03:02 +0800

    update

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -41,7 +41,7 @@ const loginRules = {
     },
     {
       pattern: /^[\u4e00-\u9fa5_a-zA-Z0-9]{3,10}$/,
-      message: "用户名必须是3~10个字母或者数字~",
+      message: "必须是3~10个字母或者数字~",
       trigger: "blur",
     },
   ],
@@ -53,7 +53,7 @@ const loginRules = {
     },
     {
       pattern: /^[a-z0-9]{3,}$/,
-      message: "用户名必须是3位以上的字母或者数字~",
+      message: "必须是3位以上的字母或者数字~",
       trigger: "blur",
     },
   ],
