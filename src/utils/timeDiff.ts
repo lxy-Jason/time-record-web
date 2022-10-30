28a39b48d0ef18754ed8b4303df951f3ac9dc52c
lxy-Jason <1627962483@qq.com>
2022-10-30 14:58:05 +0800

    暂停计时bug修复

--- src/utils/timeDiff.ts
+++ src/utils/timeDiff.ts
@@ -1,6 +1,6 @@
 import { getTotalTime } from "./getTotalTime";
 
-export const timeDiff = (start: number, end: number) => {
-  const diff = end - start;
+export const timeDiff = (start: number, end: number, pause: number) => {
+  const diff = end - start - pause;
   return getTotalTime(diff);
 };
